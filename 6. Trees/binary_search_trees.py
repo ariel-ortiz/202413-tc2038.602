8c9b319a87a45ac3cf80f5b42444c9d0bf968f95
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-09-30 13:42:57 -0600

    Add code to implement BST post_order and level_order

--- 6. Trees/binary_search_trees.py
+++ 6. Trees/binary_search_trees.py
@@ -1,6 +1,6 @@
 
 from typing import Iterator
-
+from collections import deque
 
 type Tree = list | None
 
@@ -19,6 +19,26 @@ def pre_order(root: Tree) -> Iterator:
     yield from pre_order(root[2])
 
 
+def post_order(root: Tree) -> Iterator:
+    if not root:
+        return
+    yield from post_order(root[1])
+    yield from post_order(root[2])
+    yield root[0]
+
+
+def level_order(root: Tree) -> Iterator:
+    queue: deque[Tree] = deque()
+    queue.append(root)
+    while queue:
+        current: Tree = queue.popleft()
+        if current:
+            value, left, right = current
+            yield value
+            queue.append(left)
+            queue.append(right)
+
+
 if __name__ == '__main__':
     root: Tree = [5,
                   [3,
@@ -31,3 +51,10 @@ if __name__ == '__main__':
                     None]]]
     print(list(in_order(root)))
     print(list(pre_order(root)))
+    print(list(post_order(root)))
+    print(list(level_order(root)))
+
+    for elem in in_order(root):
+        if elem % 2 == 0:
+            print(f'Found first even number: { elem }')
+            break
