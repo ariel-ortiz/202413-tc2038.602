477e9828e1bfa832327330e4c5549a4f2e38c0d2
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-10-03 14:45:33 -0600

    Add code to solve magic square using CSP

--- 7. Constraint Satisfaction Problems/magic_square.py
+++ 7. Constraint Satisfaction Problems/magic_square.py
@@ -1,8 +1,14 @@
 from itertools import permutations
-from math import factorial
+from typing import NamedTuple
+from csp import Constraint, CSP
 
 
 type Grid = list[list[int]]
+
+
+class GridLocation(NamedTuple):
+    row: int
+    column: int
 
 
 def check_square(square: Grid) -> bool:
@@ -31,5 +37,45 @@ def partition(s: list, n: int) -> list[list]:
     return [s[i:i + n] for i in range(0, len(s) // n * n, n)]
 
 
+def assignment_2_grid(assignment: dict[int, GridLocation]) -> Grid:
+    square: Grid = [[0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0]]
+    for var, (row, column) in assignment.items():
+        square[row][column] = var
+    return square
+
+
+class MagicPuzzleContraint(Constraint[int, GridLocation]):
+
+    def __init__(self, variables: list[int]):
+        super().__init__(variables)
+        self.variables: list[int] = variables
+
+    def satisfied(self, assignment: dict[int, GridLocation]) -> bool:
+        if len(assignment) != len(set(assignment.values())):
+            return False
+        if len(assignment) < 9:
+            return True
+        return check_square(assignment_2_grid(assignment))
+
+
+def solve_csp():
+    variables: list[int] = list(range(1, 10))
+    all_grid_locations: list[GridLocation] = [GridLocation(r, c) for r in range(3)
+                                                                 for c in range(3)]
+    domains: dict[int, list[GridLocation]] = {
+        var: all_grid_locations for var in variables
+    }
+    csp: CSP[int, GridLocation] = CSP(variables, domains)
+    csp.add_constraint(MagicPuzzleContraint(variables))
+    solution: dict[int, GridLocation] | None = csp.backtracking_search()
+    if solution:
+        print(assignment_2_grid(solution))
+    else:
+        print('Found no solution :(')
+
+
 if __name__ == '__main__':
     solve_brute_force()
+    solve_csp()
