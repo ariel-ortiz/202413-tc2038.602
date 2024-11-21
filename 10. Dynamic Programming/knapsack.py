b8aad911f4a4f6a59d9ad6c91e314c294bec8ff9
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-11-21 16:47:38 -0600

    Refactor code for better names and structure

--- 10. Dynamic Programming/knapsack.py
+++ 10. Dynamic Programming/knapsack.py
@@ -17,39 +17,42 @@ class Entry:
 type Table = list[list[Entry]]
 
 
-def solve(size: int, items: list[Item]) -> Table:
-    dp: Table = [[Entry(0, set()) for _ in range(size + 1)]
+EMPTY_ENTRY: Entry = Entry(0, set())
+
+
+def knapsack_table(size: int, items: list[Item]) -> Table:
+    dp: Table = [[EMPTY_ENTRY for _ in range(size + 1)]
                  for _ in range(len(items))]
-    for i in range(len(items)):
-        for j in range(1, size + 1):
-            compute_cell(items[i], dp, i, j)
+    for i, item in enumerate(items):
+        for j in range(size + 1):
+            dp[i][j] = optimal_entry(item, dp, i, j)
     return dp
 
 
-def compute_cell(item: Item, dp: Table, i: int, j: int) -> None:
+def optimal_entry(item: Item, dp: Table, i: int, j: int) -> Entry:
     if i == 0:
         if item.weight <= j:
-            dp[0][j] = Entry(item.value, {item})
-        return
-    previous_max: Entry = dp[i - 1][j]
-    dp[i][j] = previous_max
+            return Entry(item.value, {item})
+        return EMPTY_ENTRY
+    previous_max_entry: Entry = dp[i - 1][j]
     if item.weight <= j:
         remaining_space: Entry = dp[i - 1][j - item.weight]
-        current_value = item.value + remaining_space.value
-        if current_value > previous_max.value:
-            dp[i][j] = Entry(current_value, remaining_space.items | {item})
+        possible_better_value: int = item.value + remaining_space.value
+        if possible_better_value > previous_max_entry.value:
+            return Entry(possible_better_value, remaining_space.items | {item})
+    return previous_max_entry
 
 
 if __name__ == '__main__':
     from pprint import pprint
-    # dp: Table = solve(6, [Item('water', 3, 10),
-    #                       Item('book', 1, 3),
-    #                       Item('food', 2, 9),
-    #                       Item('jacket', 2, 5),
-    #                       Item('camera', 1, 6)])
-    dp: Table = solve(4, [Item('laptop', 3, 2_000),
-                          Item('iphone', 1, 2_000),
-                          Item('guitar', 1, 1_500),
-                          Item('collar', 1, 8_000),
-                          Item('stereo', 4, 3_000)])
+    # dp: Table = knapsack_table(6, [Item('water', 3, 10),
+    #                                Item('book', 1, 3),
+    #                                Item('food', 2, 9),
+    #                                Item('jacket', 2, 5),
+    #                                Item('camera', 1, 6)])
+    dp: Table = knapsack_table(4, [Item('laptop', 3, 2_000),
+                                   Item('iphone', 1, 2_000),
+                                   Item('guitar', 1, 1_500),
+                                   Item('collar', 1, 8_000),
+                                   Item('stereo', 4, 3_000)])
     pprint(dp)
